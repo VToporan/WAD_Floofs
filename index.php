799a9e5c43f1637c4f36268d8f8c8406e8b113a6
VToporan <victor.toporan@gmail.com>
2022-01-10 14:23:35 +0200

    Adds nav links to main mage

--- index.php
+++ index.php
@@ -9,8 +9,13 @@ require_once('./config.php');
     
     <body>
         <div id="content" style="margin-left:0px">
-        Index
+        Welcome to FLOOFS!
+        <div style="width:100%; height=90%;">
+        <a href="./store/store.php" class="main_nav"> Store </a>
+        <a href="./adoption/browse.php" class="main_nav"> Adoptions </a>
+        <a href="./contact.php" class="main_nav"> Contact </a>
         </div> 
+        </div>
 
 	</body>
 </html>
